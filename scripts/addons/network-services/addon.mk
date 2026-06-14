50441b0ee20b11e40993f8981f037ea2eed14bc5
Xiao <2592509183@qq.com>
2026-06-14 23:21:59 +0100

    Default network services off and flush ACM input

--- scripts/addons/network-services/addon.mk
+++ scripts/addons/network-services/addon.mk
@@ -1,6 +1,10 @@
 $(BUILDDIR)/network-services-stamp:
 	@echo "$(COLOUR_GREEN)Installing network-services helper for $(BOARD)$(END_COLOUR)"
-	@mkdir -p /rootfs/usr/bin/
+	@mkdir -p /rootfs/usr/bin/ /rootfs/etc/systemd/system/
 	@cp -a addons/network-services/network-services /rootfs/usr/bin/
 	@chmod +x /rootfs/usr/bin/network-services
+	@cp -a addons/network-services/network-services-disable.service /rootfs/etc/systemd/system/
+	@chmod 0644 /rootfs/etc/systemd/system/network-services-disable.service
+	@mkdir -p /rootfs/tmp/install/
+	@echo " network-services-disable.service" >> /rootfs/tmp/install/systemd-enable
 	@touch $@
