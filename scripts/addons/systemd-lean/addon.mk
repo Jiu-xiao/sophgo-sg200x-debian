aabafc6cef366e6853eb74d1937d29dab6114309
Xiao <2592509183@qq.com>
2026-06-15 01:37:12 +0100

    Default network services off, logind compat, and ACM input flush

--- scripts/addons/systemd-lean/addon.mk
+++ scripts/addons/systemd-lean/addon.mk
@@ -2,7 +2,9 @@ $(BUILDDIR)/systemd-lean-stamp:
 	@echo "$(COLOUR_GREEN)Installing lean systemd defaults for $(BOARD)$(END_COLOUR)"
 	@mkdir -p /rootfs/etc/systemd/system.conf.d/
 	@mkdir -p /rootfs/etc/systemd/system/systemd-udevd.service.d/
+	@mkdir -p /rootfs/etc/systemd/system/systemd-logind.service.d/
 	@cp -a addons/systemd-lean/overlay/etc/systemd/system.conf.d/*.conf /rootfs/etc/systemd/system.conf.d/
 	@cp -a addons/systemd-lean/overlay/etc/systemd/system/systemd-udevd.service.d/*.conf /rootfs/etc/systemd/system/systemd-udevd.service.d/
-	@chmod 0644 /rootfs/etc/systemd/system.conf.d/*.conf /rootfs/etc/systemd/system/systemd-udevd.service.d/*.conf
+	@cp -a addons/systemd-lean/overlay/etc/systemd/system/systemd-logind.service.d/*.conf /rootfs/etc/systemd/system/systemd-logind.service.d/
+	@chmod 0644 /rootfs/etc/systemd/system.conf.d/*.conf /rootfs/etc/systemd/system/systemd-udevd.service.d/*.conf /rootfs/etc/systemd/system/systemd-logind.service.d/*.conf
 	@touch $@
